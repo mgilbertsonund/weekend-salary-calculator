6b3f30bbead7125f2ea60e8b92e7ac3b7b3337b0
Matt Gilbertson <mgilbertsonund@gmail.com>
2024-02-18 14:31:03 -0600

    added to my submitfunction that resets all input fields to their normal state

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ console.log('hello');
 
 function submitEmployeeData(event) {
     event.preventDefault();
-    console.log('submit new employee data');
+    // console.log('submit new employee data');
 
     let firstName = document.querySelector('#first-name').value;
     let lastName = document.querySelector('#last-name').value;
@@ -10,6 +10,12 @@ function submitEmployeeData(event) {
     let jobTitle = document.querySelector('#job-title').value;
     let annualSalary = document.querySelector('#annual-salary').value;
     let employeeTable = document.querySelector('#employee-data-table');
+
+    // console.log(firstName);
+    // console.log(lastName);
+    // console.log(employeeID);
+    // console.log(jobTitle);
+    // console.log(annualSalary);
 
     employeeTable.innerHTML += `
     <tr>
@@ -22,4 +28,17 @@ function submitEmployeeData(event) {
     </tr>
     `;
 
+    // console.log(firstName);
+    // console.log(lastName);
+    // console.log(employeeID);
+    // console.log(jobTitle);
+    // console.log(annualSalary);
+
+    // these reset the form input fields to blank
+    // except for job title, it resets to index 0 or 'Select a Job'
+    document.querySelector('#first-name').value='';
+    document.querySelector('#last-name').value='';
+    document.querySelector('#id-number').value='';
+    document.querySelector('#job-title').selectedIndex = 0;
+    document.querySelector('#annual-salary').value='';
 }
