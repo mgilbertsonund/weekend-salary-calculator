2366a6629da067625423e9055fca8762c8d111fd
Matt Gilbertson <mgilbertsonund@gmail.com>
2024-02-18 14:05:51 -0600

    created a function that creates a new row, including a delete button, when employee data is submitted

--- client.js
+++ client.js
@@ -1,2 +1,25 @@
 console.log('hello');
 
+function submitEmployeeData(event) {
+    event.preventDefault();
+    console.log('submit new employee data');
+
+    let firstName = document.querySelector('#first-name').value;
+    let lastName = document.querySelector('#last-name').value;
+    let employeeID = document.querySelector('#id-number').value;
+    let jobTitle = document.querySelector('#job-title').value;
+    let annualSalary = document.querySelector('#annual-salary').value;
+    let employeeTable = document.querySelector('#employee-data-table');
+
+    employeeTable.innerHTML += `
+    <tr>
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${employeeID}</td>
+        <td>${jobTitle}</td>
+        <td>${annualSalary}</td>
+        <td><button onclick="deleteRow(event)">Delete</button></td>
+    </tr>
+    `;
+
+}
