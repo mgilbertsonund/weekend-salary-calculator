2396524b2c1fb5592a97d5f278044c0c489a0843
Matt Gilbertson <mgilbertsonund@gmail.com>
2024-02-24 18:43:02 -0600

    cleaned up JS code to utilize more functions calling on functions

--- client.js
+++ client.js
@@ -1,37 +1,56 @@
 console.log('hello');
 
 let monthlySalary = 0;
+let employeeTable = document.querySelector('#employee-data-table');
 
 function submitEmployeeData(event) {
     event.preventDefault();
-    // console.log('submit new employee data');
 
+    // initialize variables
     let firstName = document.querySelector('#first-name').value;
     let lastName = document.querySelector('#last-name').value;
     let employeeID = document.querySelector('#id-number').value;
     let jobTitle = document.querySelector('#job-title').value;
     let annualSalary = document.querySelector('#annual-salary').value;
-    // make into a function
+    
+    // display data on screen whilealso formatting salary corrertly
+    renderEmployeeData(firstName, lastName, employeeID, jobTitle, formatAnnualSalary(annualSalary));
+
+    // make salary a number again and sum into total montly budget, then calculate total monthy budget
+    annualSalary = Number(annualSalary);
+    monthlySalary = Number(monthlySalary);
+    monthlySalary += annualSalary;
+    monthlyBudget();
+
+    // clear inputs
+    clearInputFields();
+}
+
+// function that returns the average monthly cost of salary
+function monthlyBudget() {
+    let totalMonthly = Math.round(monthlySalary / 12);
+    let totalMonthlyFormatted = document.querySelector('#monthly-salary-cost');
+    totalMonthlyFormatted.innerHTML = formatAnnualSalary(totalMonthly);
+}
+
+// deletes a row when clicked
+function deleteRow(event) {
+    event.target.parentElement.parentElement.remove();
+}
+
+// formats numbers into currency
+function formatAnnualSalary(salary) {
     let annualSalaryFormatted = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
         maximumFractionDigits: 0, 
         minimumFractionDigits: 0, 
-    }).format(annualSalary);
-    let employeeTable = document.querySelector('#employee-data-table');
+    }).format(salary);
+    return annualSalaryFormatted;
+}
 
-    annualSalary = Number(annualSalary);
-    monthlySalary = Number(monthlySalary);
-    monthlySalary += annualSalary;
-    // console.log(monthlySalary);
-    monthlyBudget();
-
-    // console.log(firstName);
-    // console.log(lastName);
-    // console.log(employeeID);
-    // console.log(jobTitle);
-    // console.log(annualSalary);
-
+// creates new data in the table on the DoOM
+function renderEmployeeData(firstName, lastName, employeeID, jobTitle, annualSalaryFormatted) {
     employeeTable.innerHTML += `
     <tr>
         <td>${firstName}</td>
@@ -42,34 +61,13 @@ function submitEmployeeData(event) {
         <td><button onclick="deleteRow(event)">Delete</button></td>
     </tr>
     `;
+}
 
-    // console.log(firstName);
-    // console.log(lastName);
-    // console.log(employeeID);
-    // console.log(jobTitle);
-    // console.log(annualSalary);
-
-    // these reset the form input fields to blank
-    // except for job title, it resets to index 0 or 'Select a Job'
+// clear inputs
+function clearInputFields() {
     document.querySelector('#first-name').value='';
     document.querySelector('#last-name').value='';
     document.querySelector('#id-number').value='';
     document.querySelector('#job-title').selectedIndex = 0;
     document.querySelector('#annual-salary').value='';
-
-}
-
-function monthlyBudget() {
-    let totalMonthly = Math.round(monthlySalary / 12);
-    let totalMonthlyFormatted = document.querySelector('#monthly-salary-cost');
-    totalMonthlyFormatted.innerHTML = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        maximumFractionDigits: 0, 
-        minimumFractionDigits: 0, 
-    }).format(totalMonthly);
-}
-
-function deleteRow(event) {
-    event.target.parentElement.parentElement.remove();
 }
